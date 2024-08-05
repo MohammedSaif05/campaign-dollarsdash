023ec93964dbd84415bc49df22faf1b1885eaeaa
Maheshwaran M <85986691+mahesh1706@users.noreply.github.com>
2024-08-05 22:50:30 +0530

    upadt

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-document.querySelector('.nav-toggle').addEventListener('click', function() {
-    document.querySelector('.nav-menu').classList.toggle('active');
-});
+document.addEventListener('DOMContentLoaded', function() {
+    var toggler = document.querySelector('.navbar-toggler');
+    var collapse = document.querySelector('#navbarNav');
+  
+    // Add event listener for toggle button
+    toggler.addEventListener('click', function() {
+      if (collapse.classList.contains('show')) {
+        toggler.classList.remove('toggled');
+      } else {
+        toggler.classList.add('toggled');
+      }
+    });
+  });
+  
