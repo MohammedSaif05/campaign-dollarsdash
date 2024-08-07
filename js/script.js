e378832bf32df986857a7276049a15f973723e37
MohammedSaif05 <pianist.saif05@gmail.com>
2024-08-07 19:09:02 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var toggler = document.querySelector('.navbar-toggler');
-    var collapse = document.querySelector('#navbarNav');
-  
-    toggler.addEventListener('click', function() {
-      if (collapse.classList.contains('show')) {
-        toggler.classList.remove('toggled');
-      } else {
-        toggler.classList.add('toggled');
-      }
-    });
-  
-    // Optional: Handle the collapse event to ensure icon changes when collapsing manually
-    $(collapse).on('show.bs.collapse', function() {
-      toggler.classList.add('toggled');
-    }).on('hide.bs.collapse', function() {
-      toggler.classList.remove('toggled');
-    });
-  });
-  
 
   $('.owl-carousel').owlCarousel({
     items:1,
